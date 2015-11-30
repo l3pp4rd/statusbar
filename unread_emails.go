1f5653300aa2be11b8e6cac829cb99acb95daf20
gedi <gediminas.morkevicius@gmail.com>
2015-11-30 10:50:56 +0200

    improved unread email checks to be concurrent

--- unread_emails.go
+++ unread_emails.go
@@ -5,20 +5,56 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"sync"
 )
 
-var client = &http.Client{}
-var last_iteration = EMAIL_PER_ITERATIONS
-var last_counts []int
+type gmail struct {
+	sync.WaitGroup
+	sync.Mutex
 
-func get_unread(usr, psw string) (c int, err error) {
+	accounts []struct {
+		Username, Password string
+	}
+
+	client *http.Client
+
+	iteration int
+	counts    []int
+	results   map[string]int
+}
+
+func new_gmail_client(confPath string) (*gmail, error) {
+	gm := &gmail{
+		client:    &http.Client{},
+		iteration: EMAIL_PER_ITERATIONS - 1,
+	}
+
+	file, err := ioutil.ReadFile(confPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read gmail account config file: %s - %s", confPath, err)
+	}
+
+	if err = json.Unmarshal(file, &gm.accounts); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal gmail config file: %s - %s", confPath, err)
+	}
+
+	for _ = range gm.accounts {
+		gm.counts = append(gm.counts, 0)
+	}
+
+	gm.results = make(map[string]int, len(gm.accounts))
+	return gm, nil
+}
+
+func (gm *gmail) fetch(usr, psw string) (c int, err error) {
 	req, err := http.NewRequest("GET", EMAIL_FEED, nil)
 	if err != nil {
 		return
 	}
 	req.SetBasicAuth(usr, psw)
-	res, err := client.Do(req)
+	res, err := gm.client.Do(req)
 	if err != nil {
 		return
 	}
@@ -39,44 +75,50 @@ func get_unread(usr, psw string) (c int, err error) {
 }
 
 func unread_emails(confPath string) element {
+	gm, err := new_gmail_client(confPath)
+	if err != nil {
+		return func() (string, error) {
+			return "", err
+		}
+	}
+
 	return func() (string, error) {
-		if last_iteration < EMAIL_PER_ITERATIONS {
-			last_iteration++
-			return unread_email_representation(last_counts), nil
+		if gm.iteration < EMAIL_PER_ITERATIONS {
+			gm.iteration++
+			return gm.result(), nil
 		}
 
-		file, err := ioutil.ReadFile(confPath)
-		if err != nil {
-			return "", err
+		gm.Add(len(gm.accounts))
+		for _, acc := range gm.accounts {
+			go func(u, p string) {
+				c, err := gm.fetch(u, p)
+				if err != nil {
+					log.Println("failed to fetch email count from: %s - %s", u, err)
+					c = 0
+				}
+				gm.Lock()
+				gm.results[u] = c
+				gm.Unlock()
+				gm.Done()
+			}(acc.Username, acc.Password)
 		}
-		var accounts []struct {
-			Username, Password string
-		}
-
-		if err = json.Unmarshal(file, &accounts); err != nil {
-			return "", err
-		}
+		gm.Wait()
 
 		var counts []int
-		for _, acc := range accounts {
-			cnt, err := get_unread(acc.Username, acc.Password)
-			if err != nil {
-				return "", err
-			}
-			counts = append(counts, cnt)
+		for _, acc := range gm.accounts {
+			counts = append(counts, gm.results[acc.Username])
 		}
-
-		last_iteration = 1
-		last_counts = counts
-		return unread_email_representation(counts), nil
+		gm.counts = counts
+		gm.iteration = 1
+		return gm.result(), nil
 	}
 }
 
-func unread_email_representation(counts []int) string {
+func (gm *gmail) result() string {
 	var out string
-	if len(counts) > 0 {
+	if len(gm.counts) > 0 {
 		out = "^i(" + xbm("mail") + ")"
-		for _, c := range counts {
+		for _, c := range gm.counts {
 			if c > 0 {
 				out += fmt.Sprintf(" ^fg(#dc322f)%d^fg()", c)
 			} else {
